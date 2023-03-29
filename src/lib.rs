266895d03717286ee74ce0b5385954841c1dc2d9
imbolc <me@imbolc.name>
2023-03-29 16:09:36 +0600

    Init

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+//! # pinnacle
+//!
+//! Rust Wrapper for Pinnacle Sports API
+
+#![warn(clippy::all, missing_docs, nonstandard_style, future_incompatible)]
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
